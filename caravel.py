27a0dc0dbb3b95ad1f667808b353fcd76b0b5cc1
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2018-11-08 15:03:53 -0500

    multiple changes
    read project options from YAML that the env var points to, let the user select and read in with peppy

--- caravel.py
+++ caravel.py
@@ -6,53 +6,31 @@ import shutil
 import tempfile
 import yaml
 
-
 from flask import Flask, render_template, redirect, url_for, request
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('select.html', route="process")
+    try:  
+        project_list_path = os.path.expanduser(os.environ["CARAVEL"]); print(project_list_path)
+        assert os.path.isfile(project_list_path), "The $CARAVEL environment variable does not point to a file" 
+        with open(project_list_path, 'r') as stream:
+            pl = yaml.safe_load(stream)
+            print(pl)
+            assert "projects" in pl, "'projects' key not in the projects list file."
+            projects=pl["projects"]
+    except KeyError: 
+        print "Please set the environment variable $CARAVEL"
+        sys.exit(1)
 
+    return(render_template('index.html', projects=projects))
 
 @app.route("/process" , methods=['GET', 'POST'])
 def process():
-    
-    # Browsers aren't allowed to access the actual files stuck into HTML boxes for security reasons,
-    # so we have to actually copy those into a temporary location and read them from there.
-    # I couldn't figure out how to use the actual files; I don't think it's possible (even with javascript)
-    # because of intended security limitation of browsers that prevents them from seeing the actual file system.
-
-    uploaded_files = request.files.getlist("pconfig")
-    print(uploaded_files)
-
-    tmpdirname = tempfile.mkdtemp("tmpdir")
-    print('Created temporary directory:', tmpdirname)
-
-    pconfig = ""
-    # We have to upload multiple files (the config plus sample annotation);
-    # here we have to figure out which one ends in `yaml` -- that's the config
-    for f in uploaded_files:
-        fullname = "/".join([tmpdirname, f.filename])
-        print(fullname)
-        fn, file_extension = os.path.splitext(f.filename)
-        print(file_extension)
-        if file_extension == ".yaml":
-            pconfig = fullname
-        f.save(fullname)
-
-    print("Config file saved to: " + pconfig)
-    
-    retval=""
-    try:
-        p = peppy.Project(pconfig)
-        retval = str(p)
-        print(p)
-    except:
-        pass
-
-    # Now I delete the temporary PEP components; but this could be done later
-    
-    shutil.rmtree(tmpdirname)
-    return(retval)
+    print("in Process")
+    selected_project = request.form.get('select_project')
+    print("Loading project: " + selected_project)
+    p = peppy.Project(selected_project)
+    # print(p)
+    return(render_template('process.html', p=p))
    
